d4d5b8c294fe43ee2a83dfbf6c7c7e788e336f76
Brian Clay <bclay@fastly.com>
2022-11-03 13:10:42 -0600

    adds collapse

--- js/main.js
+++ js/main.js
@@ -7,3 +7,82 @@ $('.back-to-home').click(function(){
     $('.nav-wrapper').removeClass('show-delivery');
     $('.page-content').removeClass('show-delivery');
 })
+
+$('.collapse-nav').click(function(){
+    $('.nav-wrapper').toggleClass('collapsed');
+    $('.nav-item').toggleClass('collapsed');
+    $('body').toggleClass('nav-collapsed')
+})
+
+tippy(
+    '.nav-item.home-item', {
+        content:'Home',
+        placement: 'right',
+        animation: 'shift-away',
+        delay: 50
+    }
+)
+tippy(
+    '.nav-item.delivery-item', {
+        content:'Delivery',
+        placement: 'right',
+        animation: 'shift-away',
+        delay: 50
+    }
+)
+tippy(
+    '.nav-item.compute-item', {
+        content:'Compute',
+        placement: 'right',
+        animation: 'shift-away',
+        delay: 50
+    }
+)
+tippy(
+    '.nav-item.security-item', {
+        content:'Security',
+        placement: 'right',
+        animation: 'shift-away',
+        delay: 50
+    }
+)
+tippy(
+    '.nav-item.stats-item', {
+        content:'Stats',
+        placement: 'right',
+        animation: 'shift-away',
+        delay: 50
+    }
+)
+tippy(
+    '.nav-item.resources-item', {
+        content:'Resources',
+        placement: 'right',
+        animation: 'shift-away',
+        delay: 50
+    }
+)
+tippy(
+    '.nav-item.marketplace-item', {
+        content:'Marketplace',
+        placement: 'right',
+        animation: 'shift-away',
+        delay: 50
+    }
+)
+tippy(
+    '.nav-item.service-summary-item', {
+        content:'Service Summary',
+        placement: 'right',
+        animation: 'shift-away',
+        delay: 50
+    }
+)
+tippy(
+    '.nav-item.service-config-item', {
+        content:'Service config',
+        placement: 'right',
+        animation: 'shift-away',
+        delay: 50
+    }
+)
