b526aa9351d9f3579833b192ce0f0db4a1388c54
Brian Clay <bclay@fastly.com>
2022-11-04 14:23:47 -0600

    Features for days

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
+//Left nav
+
 $('.delivery-item').click(function(){
     $('.nav-wrapper').addClass('show-delivery');
     $('.page-content').addClass('show-delivery');
 })
 
-$('.back-to-home').click(function(){
+$('#back-to-home, #back-to-home2').click(function(){
     $('.nav-wrapper').removeClass('show-delivery');
     $('.page-content').removeClass('show-delivery');
 })
@@ -12,7 +14,48 @@ $('.collapse-nav').click(function(){
     $('.nav-wrapper').toggleClass('collapsed');
     $('.nav-item').toggleClass('collapsed');
     $('body').toggleClass('nav-collapsed')
+});
+
+//Dropdowns
+$(document).click(function(){
+    $(".dropdown-menu").removeClass('show');
+    $('.account-switcher').removeClass('show-menu');
+    $('.profile-dropdown').removeClass('show-menu');
+    $('.search-dropdown').removeClass('show-menu');
+    $('.summary-dropdown').removeClass('show-menu');
+    $('.main-nav-dropdown').removeClass('show-menu');
+});
+
+$(".dropdown-menu, .dropdown-trigger, .search, .service-summary-trigger, .main-nav-trigger").click(function(e){
+    e.stopPropagation();
+});
+
+$('.account-switcher').click(function(){
+    $('.account-menu').toggleClass('show');
+    $('.account-switcher').toggleClass('show-menu');
 })
+
+$('.profile-dropdown').click(function(){
+    $('.profile-menu').toggleClass('show');
+    $('.profile-dropdown').toggleClass('show-menu');
+})
+
+$('.search').click(function(){
+    $('.search-dropdown').toggleClass('show');
+    setTimeout(function() { $('#search-input').focus() }, 100);
+})
+
+$('.service-summary-trigger').click(function(){
+    $('.summary-dropdown').toggleClass('show');
+    $('.service-summary-trigger').toggleClass('show-menu');
+})
+
+$('.main-nav-trigger').click(function(){
+    $('.main-nav-dropdown').toggleClass('show');
+    $('.main-nav-trigger').toggleClass('show-menu');
+})
+
+//Tooltips
 
 tippy(
     '.nav-item.home-item', {
@@ -72,7 +115,7 @@ tippy(
 )
 tippy(
     '.nav-item.service-summary-item', {
-        content:'Service Summary',
+        content:'Service summary',
         placement: 'right',
         animation: 'shift-away',
         delay: 50
